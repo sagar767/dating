4a7f6d9faef90b5545e2e3252c9510c91dd26d4f
sagar767 <sagarbiswas767@gmail.com>
2016-04-03 16:39:10 +0530

    Changes in dating site first

---
+++ layouts/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php include('global.php'); ?>
+<html lang="en-US">
+<head>
+	<title>Welcome to Dating</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Free Web tutorials">
+	<meta name="keywords" content="HTML,CSS,XML,JavaScript">
+	<meta name="author" content="Hege Refsnes">
+	<link rel="icon" href="demo_icon.gif" type="image/gif" sizes="16x16">
+</head>
+<body>
+	<header>
+		<div class="site-logo">
+			<img src="<?php echo url()?>'library/images/site-logo.gif"/>
+		</div>
+	<header>
+	<footer>
+	</footer>
+</body>		
